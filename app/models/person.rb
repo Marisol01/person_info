43718ceb756627176e3a94524979629ed13fc642
Marisoln Araujo <u167272@gmail.com>
2019-03-13 17:49:04 -0600

    added controller

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,28 @@
 class Person < ApplicationRecord
+
+  validates :age, numericality: {
+    less_than_or_equal_to: 150
+  }
+
+  validates :name, :age, :alive, :gender, presence: true
+
+  def self.order_by_age
+    order(:age)
+  end
+
+  before_save :set_alive
+
+  def set_alive
+    self.alive = true
+  end
+
 end
+
+
+ 
+
+
+
+
+  
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'Faker'
+
+
+
+
+10.times do
+  Person.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    age: Faker::Number.between(1, 100),
+    hair_color: Faker::Color.color_name,
+    eye_color: Faker::Color.color_name,
+    gender: Faker::Gender.binary_type,
+    alive: true
+    )
+end
